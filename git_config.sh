cfe0dbf01a61debb4e56a4bc01ea06c7ebce9115
Vak1th <bursa.petr.f@gmail.com>
2024-10-10 17:31:16 +0200

    dsd

---
+++ git_config.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+echo "Welcome to Git config"
+git config --global user.email "bursa.petr.f@gmail.com"
+git config --global user.name "Vak1th"
+git config --list
+echo "Done successfully"
