8464a33424f1a32ead306d07757ec4e248ad92a7
Vak1th <bursa.petr.f@gmail.com>
2024-10-11 07:17:31 +0200

    ukol2

--- gsap_html/script.js
+++ gsap_html/script.js
@@ -1,6 +1,9 @@
+
 const timeline = gsap.timeline({ defaults: { duration: 1 }})
 timeline
-  .from('.header', { y: '-100%', ease: 'bounce' })
-  .from('.link', { opacity: 0, stagger: .5 })
-  .to('.footer', {  y: 0, ease: 'elastic' })
-  .fromTo('.button', { opacity: 0, scale: 0, rotation: 720 }, { opacity: 1, scale: 1, rotation: 0 })
+  .from(".header", { y: '-100%', ease: 'bounce' })
+  .from(".link", { opacity: 0, stagger: .5 })
+  .to(".footer", {  y: 0, ease: 'elastic' })
+  .fromTo(".button", { opacity: 0, scale: 0, rotation: 720 }, { opacity: 1, scale: 1, rotation: 0 })
+  .To(".circle", { opacity: 100, path: [{ x: 0, y: 0 }, { x: 200, y: -100 }, { x: 400, y: 0 }, { x: 200, y: 100 }, { x: 0, y: 0 }], curviness: 1.25, autoRotate: true} )
+  
