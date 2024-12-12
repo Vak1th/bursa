d850a92d0fce6041dc82be1e4f69a9c33149a426
Vak1th <159769911+Vak1th@users.noreply.github.com>
2024-12-12 16:03:13 +0100

    web :3

--- git_config.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-echo "Welcome to Git config"
-git config --global user.email "bursa.petr.f@gmail.com"
-git config --global user.name "Vak1th"
-git config --list
-echo "Done successfully"
